584fe362ff3e877d6f091be30482c31fb0262490
altafin <marbr72@hotmail.com>
2025-03-07 22:10:30 -0300

    Atualização de bibliotecas e teste de botão.

--- resources/views/customer/partials/person.blade.php
+++ resources/views/customer/partials/person.blade.php
@@ -7,5 +7,7 @@
         <p class="mt-1 text-sm text-gray-600">
             {{ __("Clientes cadastrados") }}
         </p>
+
+        <button class="bg-cyan-500 shadow-lg shadow-cyan-500/50 ...">Subscribe</button>
     </header>
 </section>
